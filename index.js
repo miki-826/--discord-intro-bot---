e6c3dc4470093dc728a507fcc3a1f749f2e38d9c
Miki <mickey.2003826@gmail.com>
2025-11-11 21:19:46 +0900

    add: supabase client for database integration

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 rest.put(
-  Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+  Routes.applicationCommands(process.env.CLIENT_ID),
   { body: commands }
 )
 .then(() => console.log('✅ ギルドコマンド登録完了'))
